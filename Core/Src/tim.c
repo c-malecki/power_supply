ebfa2ecbaab9c1e18d59a69b0b724ffd9a581811
chris <chris@laptop.malecki.com>
2026-02-26 13:34:32 -0500

    reconfigure some pin placement for better routing

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -201,7 +201,7 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* timHandle)
     PA9     ------> TIM1_CH2
     PA10     ------> TIM1_CH3
     */
-    GPIO_InitStruct.Pin = PWM_LED_MAIN_R_Pin|PWM_LED_MAIN_G_Pin|PWM_LED_MAIN_B_Pin;
+    GPIO_InitStruct.Pin = PWM_LED_MAIN_G_Pin|PWM_LED_MAIN_R_Pin|PWM_LED_MAIN_B_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
