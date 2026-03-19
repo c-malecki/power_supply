6b251eb68b84294210f2d9fd54f97e8a4ec282db
Chris <chrismmalecki@gmail.com>
2026-03-19 16:50:45 -0400

    working button interrupts and control in loop for turning channels on/off

--- App/tests/test_power.c
+++ App/tests/test_power.c
@@ -36,120 +36,120 @@ void Test_Power(App_t *app)
 
 // TODO: Needs PID loop because lower voltages are short of the target but
 // the voltage is on target as it gets closer to 9V+
-void test_var_chan(Power_Controller_t *ctrl)
-{
-    // variable channel is set to min during app init
-    printf("Var channel out enabled\r\n");
-    Power_Controller_EnableOut(ctrl, POWER_CHANNEL_VARIABLE, true);
-    Power_Controller_UpdateVarValues(ctrl);
-    print_var_chan(ctrl);
+// void test_var_chan(Power_Controller_t *ctrl)
+// {
+//     // variable channel is set to min during app init
+//     printf("Var channel out enabled\r\n");
+//     Power_Controller_ToggleOut(ctrl, POWER_CHANNEL_VARIABLE, true);
+//     Power_Controller_UpdateVarValues(ctrl);
+//     print_var_chan(ctrl);
 
-    HAL_Delay(2000);
+//     HAL_Delay(2000);
 
-    // test stepping up the voltage levels
+//     // test stepping up the voltage levels
 
-    Power_Controller_SetVoltage(ctrl, VOLTAGE_VARIABLE_MIN_WHOLE, VOLTAGE_VARIABLE_MIN_DECIMAL);
-    print_var_chan(ctrl);
+//     Power_Controller_SetVoltage(ctrl, VOLTAGE_VARIABLE_MIN_WHOLE, VOLTAGE_VARIABLE_MIN_DECIMAL);
+//     print_var_chan(ctrl);
 
-    HAL_Delay(2000);
+//     HAL_Delay(2000);
 
-    Power_Controller_SetVoltage(ctrl, 7, 0);
-    print_var_chan(ctrl);
+//     Power_Controller_SetVoltage(ctrl, 7, 0);
+//     print_var_chan(ctrl);
 
-    HAL_Delay(2000);
+//     HAL_Delay(2000);
 
-    Power_Controller_SetVoltage(ctrl, 8, 0);
-    print_var_chan(ctrl);
+//     Power_Controller_SetVoltage(ctrl, 8, 0);
+//     print_var_chan(ctrl);
 
-    HAL_Delay(2000);
+//     HAL_Delay(2000);
 
-    Power_Controller_SetVoltage(ctrl, 9, 0);
-    print_var_chan(ctrl);
+//     Power_Controller_SetVoltage(ctrl, 9, 0);
+//     print_var_chan(ctrl);
 
-    HAL_Delay(2000);
+//     HAL_Delay(2000);
 
-    Power_Controller_SetVoltage(ctrl, 10, 0);
-    print_var_chan(ctrl);
+//     Power_Controller_SetVoltage(ctrl, 10, 0);
+//     print_var_chan(ctrl);
 
-    HAL_Delay(2000);
+//     HAL_Delay(2000);
 
-    Power_Controller_SetVoltage(ctrl, 11, 0);
-    print_var_chan(ctrl);
+//     Power_Controller_SetVoltage(ctrl, 11, 0);
+//     print_var_chan(ctrl);
 
-    HAL_Delay(2000);
+//     HAL_Delay(2000);
 
-    Power_Controller_SetVoltage(ctrl, 12, 0);
-    print_var_chan(ctrl);
+//     Power_Controller_SetVoltage(ctrl, 12, 0);
+//     print_var_chan(ctrl);
 
-    HAL_Delay(2000);
+//     HAL_Delay(2000);
 
-    Power_Controller_SetVoltage(ctrl, 13, 0);
-    print_var_chan(ctrl);
+//     Power_Controller_SetVoltage(ctrl, 13, 0);
+//     print_var_chan(ctrl);
 
-    HAL_Delay(2000);
+//     HAL_Delay(2000);
 
-    Power_Controller_SetVoltage(ctrl, 14, 0);
-    print_var_chan(ctrl);
+//     Power_Controller_SetVoltage(ctrl, 14, 0);
+//     print_var_chan(ctrl);
 
-    HAL_Delay(2000);
+//     HAL_Delay(2000);
 
-    Power_Controller_SetVoltage(ctrl, VOLTAGE_VARIABLE_MAX_WHOLE, VOLTAGE_VARIABLE_MAX_DECIMAL);
-    print_var_chan(ctrl);
+//     Power_Controller_SetVoltage(ctrl, VOLTAGE_VARIABLE_MAX_WHOLE, VOLTAGE_VARIABLE_MAX_DECIMAL);
+//     print_var_chan(ctrl);
 
-    HAL_Delay(2000);
+//     HAL_Delay(2000);
 
-    // test moving from min up to max voltage
+//     // test moving from min up to max voltage
 
-    // min
-    Power_Controller_SetVoltage(ctrl, VOLTAGE_VARIABLE_MIN_WHOLE, VOLTAGE_VARIABLE_MIN_DECIMAL);
-    print_var_chan(ctrl);
+//     // min
+//     Power_Controller_SetVoltage(ctrl, VOLTAGE_VARIABLE_MIN_WHOLE, VOLTAGE_VARIABLE_MIN_DECIMAL);
+//     print_var_chan(ctrl);
 
-    HAL_Delay(2000);
+//     HAL_Delay(2000);
 
-    // max
-    Power_Controller_SetVoltage(ctrl, VOLTAGE_VARIABLE_MAX_WHOLE, VOLTAGE_VARIABLE_MAX_DECIMAL);
-    print_var_chan(ctrl);
+//     // max
+//     Power_Controller_SetVoltage(ctrl, VOLTAGE_VARIABLE_MAX_WHOLE, VOLTAGE_VARIABLE_MAX_DECIMAL);
+//     print_var_chan(ctrl);
 
-    HAL_Delay(2000);
+//     HAL_Delay(2000);
 
-    // min
-    Power_Controller_SetVoltage(ctrl, VOLTAGE_VARIABLE_MIN_WHOLE, VOLTAGE_VARIABLE_MIN_DECIMAL);
-    print_var_chan(ctrl);
+//     // min
+//     Power_Controller_SetVoltage(ctrl, VOLTAGE_VARIABLE_MIN_WHOLE, VOLTAGE_VARIABLE_MIN_DECIMAL);
+//     print_var_chan(ctrl);
 
-    HAL_Delay(2000);
+//     HAL_Delay(2000);
 
-    // disable again
-    Power_Controller_EnableOut(ctrl, POWER_CHANNEL_VARIABLE, false);
-    printf("Var channel out disabled\r\n");
-    print_var_chan(ctrl);
-}
+//     // disable again
+//     Power_Controller_ToggleOut(ctrl, POWER_CHANNEL_VARIABLE, false);
+//     printf("Var channel out disabled\r\n");
+//     print_var_chan(ctrl);
+// }
 
-void test_fixed_chans(Power_Controller_t *ctrl)
-{
-    // 3V3 enabled and recheck main power values
-    Power_Controller_EnableOut(ctrl, POWER_CHANNEL_3V3, true);
-    printf("3V3 channel out enabled\r\n");
-    Power_Controller_UpdateMainValues(ctrl);
-    HAL_Delay(2000);
+// void test_fixed_chans(Power_Controller_t *ctrl)
+// {
+//     // 3V3 enabled and recheck main power values
+//     Power_Controller_ToggleOut(ctrl, POWER_CHANNEL_3V3, true);
+//     printf("3V3 channel out enabled\r\n");
+//     Power_Controller_UpdateMainValues(ctrl);
+//     HAL_Delay(2000);
 
-    // 5V enabled and recheck main power values
-    Power_Controller_EnableOut(ctrl, POWER_CHANNEL_5V, true);
-    printf("5V channel out enabled\r\n");
-    Power_Controller_UpdateMainValues(ctrl);
-    HAL_Delay(2000);
+//     // 5V enabled and recheck main power values
+//     Power_Controller_ToggleOut(ctrl, POWER_CHANNEL_5V, true);
+//     printf("5V channel out enabled\r\n");
+//     Power_Controller_UpdateMainValues(ctrl);
+//     HAL_Delay(2000);
 
-    // 3V3 disabled and recheck main power values
-    printf("3V3 channel out disabled\r\n");
-    Power_Controller_EnableOut(ctrl, POWER_CHANNEL_3V3, false);
-    Power_Controller_UpdateMainValues(ctrl);
-    HAL_Delay(2000);
+//     // 3V3 disabled and recheck main power values
+//     printf("3V3 channel out disabled\r\n");
+//     Power_Controller_ToggleOut(ctrl, POWER_CHANNEL_3V3, false);
+//     Power_Controller_UpdateMainValues(ctrl);
+//     HAL_Delay(2000);
 
-    // 5V disabled and recheck main power values
-    printf("5V channel out disabled\r\n\n");
-    Power_Controller_EnableOut(ctrl, POWER_CHANNEL_5V, false);
-    Power_Controller_UpdateMainValues(ctrl);
-    HAL_Delay(2000);
-}
+//     // 5V disabled and recheck main power values
+//     printf("5V channel out disabled\r\n\n");
+//     Power_Controller_ToggleOut(ctrl, POWER_CHANNEL_5V, false);
+//     Power_Controller_UpdateMainValues(ctrl);
+//     HAL_Delay(2000);
+// }
 
 void print_power_main(Power_Controller_t *ctrl)
 {
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -20,6 +20,7 @@
 #include "main.h"
 #include "dma.h"
 #include "i2c.h"
+#include "stm32f4xx_hal.h"
 #include "tim.h"
 #include "gpio.h"
 
@@ -64,40 +65,54 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
     static uint32_t last_press_3v3 = 0;
     static uint32_t last_press_5v = 0;
     static uint32_t last_press_var = 0;
+    static uint32_t last_press_menu = 0;
     uint32_t now = HAL_GetTick();
 
     switch (GPIO_Pin) {
 
     case GPIO_BTN_VAR_Pin:
     {
-        if (now - last_press_var < 50)
+        if (now - last_press_var < 75)
             return;
         last_press_var = now;
         bool enabled = !app.power_controller.channels[POWER_CHANNEL_VARIABLE].output_enabled;
-        Power_Controller_EnableOut(&app.power_controller, POWER_CHANNEL_VARIABLE, enabled);
+        app.power_controller.channels[POWER_CHANNEL_VARIABLE].output_enabled = enabled;
+        app.power_controller.channels[POWER_CHANNEL_VARIABLE].output_pending = true;
         printf("GPIO_BTN_VAR: %u\r\n\n", enabled);
         break;
     }
 
     case GPIO_BTN_5V_Pin:
     {
-        if (now - last_press_5v < 50)
+        if (now - last_press_5v < 75)
             return;
         last_press_5v = now;
         bool enabled = !app.power_controller.channels[POWER_CHANNEL_5V].output_enabled;
-        Power_Controller_EnableOut(&app.power_controller, POWER_CHANNEL_5V, enabled);
+        app.power_controller.channels[POWER_CHANNEL_5V].output_enabled = enabled;
+        app.power_controller.channels[POWER_CHANNEL_5V].output_pending = true;
         printf("GPIO_BTN_5V: %u\r\n\n", enabled);
         break;
     }
 
     case GPIO_BTN_3V3_Pin:
     {
-        if (now - last_press_3v3 < 50)
+        if (now - last_press_3v3 < 75)
             return;
         last_press_3v3 = now;
         bool enabled = !app.power_controller.channels[POWER_CHANNEL_3V3].output_enabled;
-        Power_Controller_EnableOut(&app.power_controller, POWER_CHANNEL_3V3, enabled);
+        app.power_controller.channels[POWER_CHANNEL_3V3].output_enabled = enabled;
+        app.power_controller.channels[POWER_CHANNEL_3V3].output_pending = true;
         printf("GPIO_BTN_3V3: %u\r\n\n", enabled);
+        break;
+    }
+
+    case GPIO_BTN_MENU_Pin:
+    {
+        if (now - last_press_menu < 75)
+            return;
+        last_press_menu = now;
+        bool enabled = !app.power_controller.channels[POWER_CHANNEL_3V3].output_enabled;
+        printf("GPIO_BTN_MENU: %u\r\n\n", enabled);
         break;
     }
 
@@ -135,119 +150,129 @@ int _write(int file, char *ptr, int len)
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
 
-  /* USER CODE BEGIN 1 */
+    /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+    /* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+    /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
 
-  /* USER CODE BEGIN Init */
+    /* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+    /* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+    /* Configure the system clock */
+    SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+    /* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+    /* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_I2C1_Init();
-  MX_TIM3_Init();
-  MX_TIM2_Init();
-  MX_TIM1_Init();
-  /* USER CODE BEGIN 2 */
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_DMA_Init();
+    MX_I2C1_Init();
+    MX_TIM3_Init();
+    MX_TIM2_Init();
+    MX_TIM1_Init();
+    /* USER CODE BEGIN 2 */
     SEGGER_RTT_Init();
 
     app.state = APP_STATE_PRE_INIT;
     App_Init(&app, &hi2c1);
 
-  /* USER CODE END 2 */
+    /* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
 
     while (1) {
-    /* USER CODE END WHILE */
+        /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+        /* USER CODE BEGIN 3 */
 
-        if (app.temperature_controller.cur_state == TEMP_STATE_READY
-            && (app.temperature_controller.prev_state == TEMP_STATE_INIT
-                || app.temperature_controller.prev_state == TEMP_STATE_READ_C)) {
-            Temperature_Controller_StartCnv(&app.temperature_controller);
-        } else if (app.temperature_controller.cur_state == TEMP_STATE_CNV
-                   && app.temperature_controller.prev_state == TEMP_STATE_READY) {
-            Temperature_Controller_ReqRead(&app.temperature_controller);
-        } else if (app.temperature_controller.cur_state == TEMP_STATE_READY
-                   && app.temperature_controller.prev_state == TEMP_STATE_READ_C) {
-            printf("temp: %d\r\n", app.temperature_controller.cur_temp);
+        if (app.power_controller.channels[POWER_CHANNEL_3V3].output_pending == true) {
+            Power_Controller_ToggleOut(&app.power_controller, POWER_CHANNEL_3V3);
+        }
+        if (app.power_controller.channels[POWER_CHANNEL_5V].output_pending == true) {
+            Power_Controller_ToggleOut(&app.power_controller, POWER_CHANNEL_5V);
+        }
+        if (app.power_controller.channels[POWER_CHANNEL_VARIABLE].output_pending == true) {
+            Power_Controller_ToggleOut(&app.power_controller, POWER_CHANNEL_VARIABLE);
         }
 
-        HAL_Delay(100);
+        Power_Controller_UpdateVarValues(&app.power_controller);
+
+        // if (app.temperature_controller.cur_state == TEMP_STATE_READY
+        //     && (app.temperature_controller.prev_state == TEMP_STATE_INIT
+        //         || app.temperature_controller.prev_state == TEMP_STATE_READ_C)) {
+        //     Temperature_Controller_StartCnv(&app.temperature_controller);
+        // } else if (app.temperature_controller.cur_state == TEMP_STATE_CNV
+        //            && app.temperature_controller.prev_state == TEMP_STATE_READY) {
+        //     Temperature_Controller_ReqRead(&app.temperature_controller);
+        // } else if (app.temperature_controller.cur_state == TEMP_STATE_READY
+        //            && app.temperature_controller.prev_state == TEMP_STATE_READ_C) {
+        //     printf("temp: %d\r\n", app.temperature_controller.cur_temp);
+        // }
+
+        // HAL_Delay(100);
 
         // Test_Power_VoltageCurrent(&app);
 
         // App_Run(&app);
     }
-  /* USER CODE END 3 */
+    /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    /** Configure the main internal regulator output voltage
+     */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 12;
-  RCC_OscInitStruct.PLL.PLLN = 96;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    /** Initializes the RCC Oscillators according to the specified parameters
+     * in the RCC_OscInitTypeDef structure.
+     */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 12;
+    RCC_OscInitStruct.PLL.PLLN = 96;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 4;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    /** Initializes the CPU, AHB and APB buses clocks
+     */
+    RCC_ClkInitStruct.ClockType =
+        RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK) {
+        Error_Handler();
+    }
 }
 
 /* USER CODE BEGIN 4 */
@@ -279,30 +304,30 @@ void I2C_ResetBus(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
+    /* USER CODE BEGIN Error_Handler_Debug */
     /* User can add his own implementation to report the HAL error return state */
     __disable_irq();
     while (1) { }
-  /* USER CODE END Error_Handler_Debug */
+    /* USER CODE END Error_Handler_Debug */
 }
 #ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
+    /* USER CODE BEGIN 6 */
     /* User can add his own implementation to report the file name and line number,
        ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+    /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
